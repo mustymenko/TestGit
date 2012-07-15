0556737080d79e5759b424e28587f7d0d527ed6e
Ustymenko Maksym <m.ustymenko@gmail.com>
2012-07-15 20:55:13 +0300

    Added comments to test git merge master with branch

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "main.h"
 
 int main( int argc, char * argv[] ) {
+    // Call class from main.h header file
     MainClass mC;
     mC.printProgNameWithArgs( argv, argc );
     return true;
